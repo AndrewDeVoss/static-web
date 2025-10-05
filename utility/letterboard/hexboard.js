aaf81b0fb15e3797c8f8e74ecf13d92fe33dff6c
DESKTOP-03J30FP\apdev <apdevoss99@gmail.com>
2025-10-05 16:26:55 -0500

    letter board smaller

--- utility/letterboard/hexboard.js
+++ utility/letterboard/hexboard.js
@@ -10,7 +10,7 @@ class HexBoard extends LetterBoard {
     connectedCallback() {
         const attr = this.getAttribute('letters');
         this.letters = attr ? [...attr] : [];
-        const computed = Math.min(50, window.innerWidth / 13.8564);
+        const computed = Math.min(50, window.innerWidth / 17);
         this.style.setProperty('--hex-side-length', `${computed}px`);
 
         // Set shadow DOM HTML first — so we don't wipe out styles later
